c0151d47f372767710e8757ca1d09bdd5abdec92
bitmessagedev <bitmessagedev@proton.me>
2024-12-20 03:49:03 +0000

    Perform comparisons using integers rather than bytestrings

--- src/tests/test_OpenSSL.py
+++ src/tests/test_OpenSSL.py
@@ -3,23 +3,27 @@ from unittest import TestCase
 
 from OpenSSL import OpenSSL
 
-p = unhexlify('FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEFFFFFC2F')
-order = unhexlify('FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141')
-zero_256bit = b'\x00' * 32
+p = 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEFFFFFC2F
+order = 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141
 
 class TestOpenSSL(TestCase):
   def test_generate_random_key_pair(self):
     private_key, public_key = OpenSSL.generate_random_key_pair()
-    
+
+    private_key_int = int.from_bytes(private_key)
     #private key should be between 1 and order - 1 inclusive
-    self.assertGreater(private_key, b'\x00')
-    self.assertLess(private_key, order)
+    self.assertGreater(private_key_int, 0)
+    self.assertLess(private_key_int, order)
     
     #public key should be 0x04 || X || Y
-    #both X and Y should be exactly 256 bits between 0 and p - 1 inclusive
+    #both X and Y should be between 0 and p - 1 inclusive
     self.assertEqual(len(public_key), 65)
     self.assertEqual(public_key[0], 4)
-    self.assertGreaterEqual(public_key[1:33], zero_256bit)
-    self.assertLess(public_key[1:33], p)
-    self.assertGreaterEqual(public_key[33:65], zero_256bit)
-    self.assertLess(public_key[33:65], p)
+    
+    public_key_x = int.from_bytes(public_key[1:33])
+    self.assertGreaterEqual(public_key_x, 0)
+    self.assertLess(public_key_x, p)
+    
+    public_key_y = int.from_bytes(public_key[33:65])
+    self.assertGreaterEqual(public_key_y, 0)
+    self.assertLess(public_key_y, p)
